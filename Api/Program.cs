cd10517cba729803c9de1db097ed958bcb682f2e
udaymathur14 <udaymathur14@gmail.com>
2026-04-01 17:52:33 +0530

    cqrs corrected

--- Api/Program.cs
+++ Api/Program.cs
@@ -22,7 +22,8 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "http://localhost:4200"
+                "http://localhost:4200",
+                "http://localhost:5173"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
